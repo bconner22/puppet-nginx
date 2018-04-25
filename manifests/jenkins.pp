c736598a755d4e8d6fbeac0d1c8e8453deb0272d
Brian Conner <bconner22@gmail.com>
2018-04-25 17:04:59 -0500

    fixing stuff

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,9 +1,9 @@
-exec {'wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -':
+exec {'wget dat key':
+  command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -',
 }->
-exec {'sudo sh -c \'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list\'':
+exec {'/usr/bin/sudo sh -c \'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list\'':
 }->
-exec {'sudo apt-get update:
+exec {'/usr/bin/sudo apt-get update':
 }->
-package {'jenkins':
-  ensure => present,
+exec {'usr/bin/sudo apt-get install jenkins':
 }
