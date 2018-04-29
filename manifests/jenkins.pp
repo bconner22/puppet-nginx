b3f2bc52afc4438c4d5dd1f19d8a4f558c34bc43
Brian <bconner22@gmail.com>
2018-04-29 17:38:24 -0500

    Delete jenkins.pp

--- manifests/jenkins.pp
+++
@@ -1,9 +0,0 @@
-exec {'wget dat key':
-  command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -',
-}->
-exec {'/usr/bin/sudo sh -c \'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list\'':
-}->
-exec {'/usr/bin/sudo apt-get update':
-}->
-exec {'usr/bin/sudo apt-get install jenkins':
-}
