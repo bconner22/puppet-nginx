0f7321876da34613ef6bc9a0fa4adae8251c04c3
Brian <bconner22@gmail.com>
2018-04-29 17:55:45 -0500

    Add files via upload

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,9 +1,12 @@
 package {'nginx':
   ensure => installed,
 }
+if !defined('$indexhtml') {
+  $indexhtml = "Automation for the people"
+}
 file {'/var/www/html/index.nginx-debian.html':
   ensure  => file,
-  content => 'Automation for the people',
+  content => "${indexhtml}",
 }~>
 service {'nginx':
   ensure => running,
